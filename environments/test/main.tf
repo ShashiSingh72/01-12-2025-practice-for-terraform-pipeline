bc918874ccb10467a2a49ef70d34403bfd805ec0
shashisingh72 <shashisingh223344@gmail.com>
2025-12-10 03:14:17 +0530

    multi env added

--- environments/test/main.tf
+++ environments/test/main.tf
@@ -1,27 +1,27 @@
 module "rg" {
-  source = "../../modules/rg"
+  source = "../../modules/Resource_group"
   rgs    = var.rgs
 }
 
 module "stg" {
   depends_on = [module.rg]
-  source     = "../../modules/stg"
+  source     = "../../modules/Storage_account"
   stgs       = var.stgs
 }
 module "vnet" {
   depends_on = [module.rg]
-  source     = "../../modules/vnet"
+  source     = "../../modules/Virtual_network"
   vnets      = var.vnets
   
 }
 module "subnet" {
   depends_on = [module.vnet , module.rg]
-  source     = "../../modules/subnet"
+  source     = "../../modules/Subnet"
   subnets    = var.subnets
 }
 
 module "nic" {
   depends_on = [ module.rg, module.vnet,module.subnet]
-  source = "../../modules/nic"
+  source = "../../modules/Network_interface_card"
   nics   = var.nics
 }
